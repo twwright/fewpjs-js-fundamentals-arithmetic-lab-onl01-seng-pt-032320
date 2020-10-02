ae3b784c163175f2334bbe9eb1486a2b8ab9b3db
Thomas Wright <twwright@gmail.com>
2020-10-02 13:57:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-
-// Write your code here
+let [num1, num2] = [31, 2];
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * Infinity) + 1;
+let mod = 16 % 6;
+let max = Math.max(0, 10, 20);
